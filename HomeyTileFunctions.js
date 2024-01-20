bcb63add4449291373638279fdf9ce6555c92b9f
oepi-loepi <rob@oostveen123.nl>
2024-01-20 10:32:15 +0100

    toonHomey 1.0.36
    1.0.36
    - error solved in tile creation
    - systrayicon added

--- HomeyTileFunctions.js
+++ HomeyTileFunctions.js
@@ -40,70 +40,87 @@
 		}
         xhr.send();
 	}
-
+	
 	
 	function checkIfTilesNeeded(){
-		console.log("*********homey checkIfTilesNeeded() started ")
 		var appfileString =  appFile.read()
-		if (debugOutput) console.log("*********homey appfileString: " + appfileString)
+		//if (debugOutput) console.log("*********homey appfileString: " + appfileString)
 		if(appfileString.indexOf("//LEEG") >-1){
-			console.log("*********homey //LEEG found ")
+			if (debugOutput) console.log("*********homey //LEEG found ")
 			if(tilesJSON.length>0){
 				if (debugOutput) console.log("*********homey tilesJSON.length>0 ")
-				HomeyTileFunctions.checkAndCreateTiles(tilesJSON,true)
+				HomeyTileFunctions.createTiles(tilesJSON)
+				appfileString = appfileString.replace('//LEEG','')
+				appFile.write(appfileString)
+				if (debugOutput) console.log("*********homey restarting from init() ")
+				rebootTimer.running = true
 			}else {
 				if (debugOutput) console.log("*********homey tilesJSON.length<1 ")
 				appfileString = appfileString.replace('//LEEG','')
 				appFile.write(appfileString)
 			}
-		}else{
-			if (debugOutput) console.log("*********homey //LEEG not found ")
-			HomeyTileFunctions.checkAndCreateTiles(tilesJSON,false)	
 		}
     }
-	
 
-	function checkAndCreateTiles(devicesTileArray, isLeeg){
-		if (debugOutput) console.log("*********homey checkAndCreateTiles() leeg: " + isLeeg)
+	function createTiles(devicesTileArray){
+	//This function needs to be last in the app!!!!!!!!!!
+		if (debugOutput) console.log("*********homey  createTiles()")
+		var tileString0 = ""
+		var tileString2 = ""
+		var tileString3 = ""
+		var tileString4 = "		running: (\n"
+		var firsttile = true;
+		var tilecounter = 0;
+
+
+		for(var i in devicesTileArray){
+			if(devicesTileArray[i].devflow !="leeg"){
+				tileString0 += "		registry.registerWidget(\"tile\", tileUrl" + i + ", this, null, {thumbLabel: qsTr(\"Homey_" + i + "\"), thumbIcon: thumbnailIcon, thumbCategory: \"general\", thumbWeight: 30, baseTileWeight: 10, thumbIconVAlignment: \"center\"})\n"
+				tileString2 += "	property url 	tileUrl" + i + " : \"HomeyNr" + i + "Tile.qml\"\n"
+				tileString3 += "	property bool 	tile" + i + "visible: false\n"
+				if (firsttile){
+					firsttile = false;
+					tileString4 += "			tile" + i + "visible"
+				}else{
+					tileString4 += "||\n 			tile" + i + "visible"
+				}
+				tilecounter ++
+			}
+		}
 		
+		tileString4 += ")"
+		if (tilecounter==0){tileString4 = "		running: false"}
+
+
 		var appfileString =  appFile.read()
+		//if (debugOutput) console.log("*********toonTemp old appfileString: " + appfileString)
 		var oldappfileString = appfileString
 
 		var n201 = oldappfileString.indexOf('//TILE//') + '//TILE//'.length
 		var n202 = oldappfileString.indexOf('//TILE END//',n201)
-		var oldWidgetString = oldappfileString.substring(n201, n202)
-		if (debugOutput) console.log("*********homey oldWidgetString: " + oldWidgetString)
-
-		if (!isLeeg){
-			if (debugOutput) console.log("*********homey checking tiles")
-			var needUpdate = false
-			for(var i in devicesTileArray){
-				if(devicesTileArray[i].devflow !="leeg" ){
-					if(oldWidgetString.indexOf("tileUrl" + i + ", this") < 0){
-						needUpdate = true
-					}
-				}
-			}
-		}
+		//if (debugOutput) console.log("*********homey old WidgetSettings: " + oldappfileString.substring(n201, n202))
+		var newappfileString = oldappfileString.substring(0, n201) + "\n" + tileString0 + "\n" + oldappfileString.substring(n202, oldappfileString.length)
 		
-		if(needUpdate || isLeeg){
-			if (debugOutput) console.log("*********homey creating tiles")
-			var tileString0 = ""
-			for(var i in devicesTileArray){
-				if(devicesTileArray[i].devflow !="leeg"){
-					tileString0 += "		registry.registerWidget(\"tile\", tileUrl" + i + ", this, null, {thumbLabel: qsTr(\"Homey_" + i + "\"), thumbIcon: thumbnailIcon, thumbCategory: \"general\", thumbWeight: 30, baseTileWeight: 10, thumbIconVAlignment: \"center\"})\n"
-				}
-			}
-			if (debugOutput) console.log("*********homey tileString0" + tileString0)
-			var newappfileString = oldappfileString.substring(0, n201) + "\n" + tileString0 + "\n" + oldappfileString.substring(n202, oldappfileString.length)
-			newappfileString = newappfileString.replace('//LEEG','')
-			appFile.write(newappfileString)
-			if (debugOutput) console.log("*********homey new WidgetSettings saved ")
-			if (debugOutput) console.log("*********homey restarting.............. ")
-			sleep(500);
-			Qt.quit()
-		}else{
-			if (debugOutput) console.log("*********homey creating tiles not needed")
-		}
-	
+		
+		oldappfileString = newappfileString
+		n201 = oldappfileString.indexOf('//PROPERTY//') + '//PROPERTY//'.length
+		n202 = oldappfileString.indexOf('//PROPERTY END//',n201)
+		//if (debugOutput) console.log("*********homey old WidgetSettings: " + oldappfileString.substring(n201, n202))
+		newappfileString = oldappfileString.substring(0, n201) + "\n" + tileString2 + "\n" + oldappfileString.substring(n202, oldappfileString.length)
+
+		oldappfileString = newappfileString
+		n201 = oldappfileString.indexOf('//VISIBLE//') + '//VISIBLE//'.length
+		n202 = oldappfileString.indexOf('//VISIBLE END//',n201)
+		//if (debugOutput) console.log("*********homey old WidgetSettings: " + oldappfileString.substring(n201, n202))
+		newappfileString = oldappfileString.substring(0, n201) + "\n" + tileString3 + "\n" + oldappfileString.substring(n202, oldappfileString.length)
+		
+		oldappfileString = newappfileString
+		n201 = oldappfileString.indexOf('//TIMER//') + '//TIMER//'.length
+		n202 = oldappfileString.indexOf('//TIMER END//',n201)
+		//if (debugOutput) console.log("*********homey old WidgetSettings: " + oldappfileString.substring(n201, n202))
+		newappfileString = oldappfileString.substring(0, n201) + "\n" + tileString4 + "\n" + oldappfileString.substring(n202, oldappfileString.length)
+
+		appFile.write(newappfileString)
+		if (debugOutput) console.log("*********homey new WidgetSettings saved ")
 	}
+	
